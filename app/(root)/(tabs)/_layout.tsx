6570d276579bc5820d1e8af507ed1ab8c074a1e1
Borngreat Ikwutah <iagocech2056@gmail.com>
2025-05-27 10:17:51 +0100

    feat: updated Home Screens...

---
+++ app/(root)/(tabs)/_layout.tsx
@@ -0,0 +1,85 @@
+import { Tabs } from "expo-router";
+import { Image, Text, View } from "react-native";
+import icons from "~/constants/icons";
+
+const TabIcon = ({
+  focused,
+  icon,
+  title,
+}: {
+  focused: boolean;
+  icon: any;
+  title: string;
+}) => {
+  return (
+    <View className="flex-1 items-center justify-center mt-3 flex flex-col">
+      <Image
+        source={icon}
+        tintColor={`${focused ? "#0061ff" : "#666876"}`}
+        resizeMode="contain"
+        className="size-6"
+      />
+
+      <Text
+        className={`${
+          focused
+            ? "text-primary-300 font-rubik-medium"
+            : "text-black-200 font-rubik"
+        } text-xs w-full text-center mt-1`}
+      >
+        {title}
+      </Text>
+    </View>
+  );
+};
+
+const TabsLayout = () => {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "white",
+          position: "absolute",
+          borderTopColor: "#00061FF1A",
+          borderTopWidth: 1,
+          minHeight: 80,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.home} title="Home" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          headerShown: false,
+          title: "Explore",
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.search} title="Explore" />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.person} title="Profile" />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default TabsLayout;
